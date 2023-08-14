6a6d80912bb2e96c681b43ca85aca21295a2d5c8
juancabana <juand.cabanat@unac.edu.co>
2023-08-14 14:24:18 -0500

    enviroments variables

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import express from "express";
 // Enviroment variables
-import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-dotenv.config()
+
 // Cors
 import cors from 'cors'
 // Router 
--- libs/sequelize.js
+++ libs/sequelize.js
@@ -1,17 +1,15 @@
 import { Sequelize } from 'sequelize';
-import config from '../config/config.js';
 import { setupModels } from './../models/index.js';
+import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config()
 
-// const DBUSER = encodeURIComponent(config.user);
-// const DBPASSWORD = encodeURIComponent(config.password);
-// const DBHOST = encodeURIComponent(config.dbHost);
-// const DBPORT = encodeURIComponent(config.port);
-// const DBNAME = encodeURIComponent(config.database);
-// const URI = `mysql://${DBUSER}:${DBPASSWORD}@${DBHOST}:${DBPORT}/${DBNAME}`;
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_NAME = process.env.DB_NAME;
 
 // Conexion with database
 // Pool and sequelize (ORM)
-const sequelize = new Sequelize('energysolution', 'dbadmin', 'Password123', {
+const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   host: 'localhost',
   dialect: 'postgres',
   logging: true,
